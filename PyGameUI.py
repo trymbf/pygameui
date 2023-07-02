f42af8460186317c578376a3242881ad2f9f4c9f
TBFMcDuck <trym.mcduck@gmail.com>
2023-07-02 22:12:01 +0200

    Added input as an elemnt type
    Added input, an element you can input text in to, and updated exampleUser to use this feature.

--- PyGameUI.py
+++ PyGameUI.py
@@ -302,3 +302,181 @@ class element():
         # Activate jumping
         self.jumping = True
 
+class input():
+    def __init__(self, position: tuple, **extra ):
+        # Extras are addition arguments the user can input to further customize the text
+        # The values in the array underneath are the values used if the user does not specify them
+        self.e = {"font": "freesansbold.ttf", "fontSize": 30, "exampleContent": "Click me to input!", "prefilledContent": "", "characterLimit": 12, "normalTextColor": (231, 111, 81), "exampleTextColor": (100, 100, 100), "rectWidth": 200, "rectHeight": 50, "rectColorActive": (233, 196, 106), "rectColorPassive": (200, 200, 200), "rectBorderRadius": 1, "rectBorderWidth": 5, "centerMode": True}
+        for key in extra:
+            if key in self.e:
+                self.e[key] = extra[key]
+            else: # Sends an error if the function has been passed a non recognizable argument
+                extras_keys = []
+                for extraKey in self.e:
+                    extras_keys.append(extraKey)
+                error = f"Argument error: Argument '{key}' not recognized. The extra arguments available are: {extras_keys}"
+                raise Exception(error)
+        
+        # Create position variables and font
+        # Pos
+        self.x, self.y = (position[0] - (self.e["rectWidth"] // 2), position[1] - (self.e["rectHeight"] // 2)) if self.e["centerMode"] else (position[0], position[1])
+        # Rect
+        self.rect = pygame.rect.Rect(self.x, self.y, self.e["rectWidth"], self.e["rectHeight"])
+        self.borderRadius = self.e["rectBorderRadius"]
+        self.rectBorderWidth = self.e["rectBorderWidth"]
+        self.rectColorPassive = self.e["rectColorPassive"]
+        self.rectColorActive = self.e["rectColorActive"]
+        # Text
+        self.normalTextColor = self.e["normalTextColor"]
+        self.exampleTextColor = self.e["exampleTextColor"]
+        self.characterLimit = self.e["characterLimit"]
+        self.font = pygame.font.SysFont(self.e["font"], self.e["fontSize"])  # Load font
+        self.userText = self.e["prefilledContent"]
+        self.exampleContent = self.e["exampleContent"]
+        self.userTextSurface = self.font.render(self.userText, True, self.normalTextColor) # Create surface object for the userText
+        self.exampleTextSurface = self.font.render(self.exampleContent, True, self.exampleTextColor) # Create surface object the exampleText
+
+        self.userTextRect = self.userTextSurface.get_rect() # Get rect
+        self.exampleTextRect = self.exampleTextSurface.get_rect() # Get rect
+        # centering the text
+        self.userTextRect.center = self.rect.center
+        self.exampleTextRect.center = self.rect.center
+        
+        # Show
+        self.show = True
+        # Flowing
+        self.flowing = False
+        self.currentFlowPos = None
+        self.otherFlowPos = None
+        self.Xstep = 0
+        self.Ystep = 0
+        # Jumping
+        self.jumping = False
+        self.currentJumpPos = None
+        self.otherJumpPos = None
+        self.frames = 0
+        self.frames_counter = 0
+        # Clicking
+        self.clicked = False
+        self.active = False
+
+    def draw(self, win):
+        if self.show:
+            if self.userText != "":
+                win.blit(self.userTextSurface, self.userTextRect)
+            else:
+                if not self.active:
+                    win.blit(self.exampleTextSurface, self.exampleTextRect)
+                    
+            if self.active:
+                pygame.draw.rect(win, self.rectColorActive, self.rect, self.rectBorderWidth, border_radius = self.borderRadius)
+            else:
+                pygame.draw.rect(win, self.rectColorPassive, self.rect, self.rectBorderWidth, border_radius = self.borderRadius)
+
+    def work(self, events: list, clickable_elements: list):
+        # Make activating work
+        # Get mouse pos
+        mouse_pos = pygame.mouse.get_pos()
+
+        # Check mouse over and clicked conditions
+        if self.rect.collidepoint(mouse_pos):
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False: # == 1 is left click
+                self.active = True
+                self.clicked = True
+        else:
+            if pygame.mouse.get_pressed()[0]: # == 1 is left click
+                self.active = False
+            for element in clickable_elements:
+                if element.rect.collidepoint(mouse_pos):
+                    break
+            else:
+                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+        
+        if pygame.mouse.get_pressed()[0] == 0: #  No mousebuttons down
+            self.clicked = False
+
+        # Make writing work
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if self.active:
+                    if event.key == pygame.K_BACKSPACE:
+                        self.userText = self.userText[0: -1] # Removes last character
+                    elif len(self.userText) <= self.characterLimit: # Keep text under character limit
+                        self.userText += event.unicode # Adds the userinput to the text
+                    self.userTextSurface = self.font.render(self.userText, True, self.normalTextColor) # Create surface object for the userText
+                    self.userTextRect = self.userTextSurface.get_rect() # Get rect
+                    self.userTextRect.center = self.rect.center
+
+    # Updates and moves the text if needed
+    def update(self):
+        if self.flowing: # If flowing
+            # Check if flow has reached a flowpoint
+            if (self.Xstep == 0 or self.Ystep == 0):
+                if ((self.Xstep > 0) and (self.rect.x > self.currentFlowPos[0])) or ((self.Xstep < 0) and (self.rect.x < self.currentFlowPos[0])) or ((self.Ystep < 0) and (self.rect.y < self.currentFlowPos[1])) or ((self.Ystep > 0) and (self.rect.y > self.currentFlowPos[1])):
+                    self.Xstep, self.Ystep = -self.Xstep, -self.Ystep # Switch direction
+                    self.currentFlowPos, self.otherFlowPos = self.otherFlowPos, self.currentFlowPos
+            else:
+                if (((self.Xstep > 0) and (self.rect.x > self.currentFlowPos[0])) and ((self.Ystep > 0) and (self.rect.y > self.currentFlowPos[1]))) or (((self.Xstep > 0) and (self.rect.x > self.currentFlowPos[0])) and ((self.Ystep < 0) and (self.rect.y < self.currentFlowPos[1]))) or (((self.Xstep < 0) and (self.rect.x < self.currentFlowPos[0])) and ((self.Ystep > 0) and (self.rect.y > self.currentFlowPos[1])))or (((self.Xstep < 0) and (self.rect.x < self.currentFlowPos[0])) and ((self.Ystep < 0) and (self.rect.y < self.currentFlowPos[1]))):
+                    self.Xstep, self.Ystep = -self.Xstep, -self.Ystep # Switch direction
+                    self.currentFlowPos, self.otherFlowPos = self.otherFlowPos, self.currentFlowPos
+                
+            self.move(self.Xstep, self.Ystep) # Apply movement
+        
+        elif self.jumping: # If jumping
+            self.frames_counter += 1 # Increase frame counter
+            if self.frames_counter >= self.frames: # If it has gone enough time to switch position
+                self.move_to(self.otherJumpPos[0], self.otherJumpPos[1]) # Apply movement
+                self.currentJumpPos, self.otherJumpPos = self.otherJumpPos, self.currentJumpPos
+                self.frames_counter = 0 # Reset counter
+
+    # Moves to specific cordinates
+    def move_to(self, x: int, y:  int):
+        if self.e["centerMode"]:
+            self.rect.center = (x, y)
+            self.x, self.y = self.rect.center
+            self.userTextRect.center = (x, y)
+            self.exampleTextRect.center = (x, y)
+        else:
+            self.rect.topleft = (self.x, self.y)
+            self.x, self.y = self.rect.topleft
+            self.userTextRect.topleft = (self.x, self.y)
+            self.exampleTextRect.topleft = (self.x, self.y)
+    
+    # Moves in x and y direction
+    def move(self, xMovement: float, yMovement: float):
+        # Used to make small movements posible (self.x is a float, self.textRect.x is an int)
+        self.x += xMovement
+        self.y += yMovement
+        if self.e["centerMode"]:
+            self.rect.center = (self.x, self.y)
+            self.userTextRect.center = (self.x, self.y)
+            self.exampleTextRect.center = (self.x, self.y)
+        else:
+            self.rect.topleft = (self.x, self.y)
+            self.userTextRect.topleft = (self.x, self.y)
+            self.exampleTextRect.topleft = (self.x, self.y)
+
+    # Flow lets the text flow between two points
+    def flow(self, position1: tuple, position2: tuple, iterations: int):
+        # Move to flowpoint
+        self.move_to(position1[0], position1[1])
+        # Set flowpostions
+        self.otherFlowPos, self.currentFlowPos = position1, position2 
+        # Get amount to move per iteration
+        Xdistance, Ydistance = (position2[0] - position1[0]), (position2[1] - position1[1])
+        self.Xstep = Xdistance / iterations
+        self.Ystep = Ydistance / iterations
+        # Activate flowing
+        self.flowing = True
+    
+    # Jump lets the user toggle the text between two points on a userSpecified timer
+    def jump(self, position1: tuple, position2: tuple, frames: int):
+        # Move to jumppoint
+        self.move_to(position1[0], position1[1])
+        # Set jumpPostions
+        self.otherJumpPos, self.currentJumpPos = position2, position1
+        # Frames
+        self.frames = frames
+        # Activate jumping
+        self.jumping = True
--- exampleUser.py
+++ exampleUser.py
@@ -14,7 +14,7 @@ def draw(win):
     for element in text_elements:
         element.draw(win)
         element.update()
-    for element in button_elements:
+    for element in clickable_elements:
         element.draw(win)
         element.update()
 
@@ -28,26 +28,28 @@ text_elements.append(title)
 undertitle.jump((450, 150), (450, 155), 40)
 text_elements.append(undertitle)
 # Button elements
-button_elements = []
+clickable_elements = []
 start_button = PGU.element((450, 300), rectWidth = 300, content = "Start!", rectColor = (233, 196, 106), textColor = (231, 111, 81))
 start_button.flow((450, 305), (450, 315), 60)
-button_elements.append(start_button)
-sunset = pygame.transform.scale(pygame.image.load("sunset.png"), (100, 100)).convert()
-sunset_button = PGU.element((10, 490), content = sunset, centerMode = False)
-sunset_button.flow((10, 490), (30, 470), 60)
-button_elements.append(sunset_button)
+clickable_elements.append(start_button)
+
+input_element = PGU.input((500, 500))
+input_element.flow((500, 500), (300, 300), 50)
+clickable_elements.append(input_element)
 
 clock = pygame.time.Clock()
 while True:
-    for event in pygame.event.get():
+    events = pygame.event.get()
+    for event in events:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    if start_button.was_clicked(button_elements):
-        undertitle.show = not undertitle.show
-    if sunset_button.was_clicked(button_elements):
-        sunset_button.flowing = not sunset_button.flowing
+        
 
+    if start_button.was_clicked(clickable_elements):
+        undertitle.show = not undertitle.show
+    
+    input_element.work(events, clickable_elements)
     draw(win)
     clock.tick(60)
