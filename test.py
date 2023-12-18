0cead3f4a87f9a8dfe5a4ed5f849373ec953a679
TBFMcDuck <trym.mcduck@gmail.com>
2023-12-18 13:19:47 +0100

    Small update to documentation

--- test.py
+++ test.py
@@ -4,17 +4,29 @@ pygame.init()
 
 win = pygame.display.set_mode((500, 500))
 
-example_element = PyGameUI.element((300, 300), content= 10)
+example_element = PyGameUI.element((300, 300), content= "Kul knapp")
+example_element.flow((300, 300), (300, 310), 10)
+
+Titel = PyGameUI.text((100, 100), "Titel", (255, 255, 255))
 
 clock = pygame.time.Clock()
 while True:
     win.fill((0, 0, 0))
-    for event in pygame.event.get():
+    events = pygame.event.get()
+    for event in events:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
     
     example_element.draw(win)
+    Titel.draw(win)
+    if example_element.is_hovered():
+        example_element.update()
+
+    if example_element.was_clicked([example_element]):
+        Titel.show = not Titel.show
+
+    
 
     pygame.display.flip()
     clock.tick(60)
